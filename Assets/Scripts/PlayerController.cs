2d77f6225df3d816d8d11efe89de5b9673a0cb6f
wookiepeter <matthis.hagen@o2online.de>
2016-03-09 13:32:14 +0100

    added a simple player model and a simple PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D Rb2d;
+    public float Speed;
+    Vector2 MoveDirection;
+
+	// Use this for initialization
+	void Start () {
+
+        Rb2d = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        MoveDirection = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveDirection += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            MoveDirection += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveDirection += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveDirection += Vector2.right;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Rb2d.velocity = MoveDirection.normalized * Speed; 
+
+    }
+}
