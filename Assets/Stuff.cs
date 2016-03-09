8ebcd6b76d8b5305d962e5cceba2c15bb0808ff2
HanHangit <clonkcool@yahoo.de>
2016-03-09 14:17:17 +0100

    Inventory

---
+++ Assets/Stuff.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Stuff : MonoBehaviour {
+
+    public abstract void Use();
+    public abstract string GetName();
+    public abstract bool Equipable();
+    public abstract Stuff HardCopy();
+}
