cf0804e31fbc97ffd0dd0a2bf4e24cfaeb681614
wookiepeter <matthis.hagen@o2online.de>
2016-03-09 14:08:10 +0100

    added Enemy01 and Enemy01Controller

---
+++ Assets/Scripts/Enemy01Controller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy01Controller : MonoBehaviour
+{
+
+    Rigidbody2D Rb2d;
+    public float Speed;
+    Vector2 PlayerPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        PlayerPosition = new Vector2(GameObject.FindGameObjectWithTag("Player").transform.position.x, GameObject.FindGameObjectWithTag("Player").transform.position.y);
+        
+    }
+
+    void FixedUpdate()
+    {
+        Rb2d.velocity = (PlayerPosition - new Vector2(transform.position.x, transform.position.y)).normalized * Speed;
+
+    }
+}
